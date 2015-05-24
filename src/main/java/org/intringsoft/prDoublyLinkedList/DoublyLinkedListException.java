fb0a6da7de9b9f81969bb492e6e973365898349c
agprados <aidagp@uma.es>
2015-05-24 13:55:46 +0200

    Cambios grandes en el codigo

---
+++ src/main/java/org/intringsoft/prDoublyLinkedList/DoublyLinkedListException.java
@@ -0,0 +1,13 @@
+package org.intringsoft.prDoublyLinkedList;
+
+public class DoublyLinkedListException extends Exception {
+	
+	public DoublyLinkedListException() {
+		super();
+	}
+	
+	public DoublyLinkedListException(String msg) {
+		super(msg);
+	}
+
+}
