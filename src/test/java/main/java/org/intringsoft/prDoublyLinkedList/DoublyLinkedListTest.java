c2e794aba3d777e054a696e97132e540b0b0a03b
Serg4554 <Sergio.357.95@gmail.com>
2015-05-26 00:27:22 +0200

    jUnit tests updated

--- src/test/java/main/java/org/intringsoft/prDoublyLinkedList/DoublyLinkedListTest.java
+++ src/test/java/main/java/org/intringsoft/prDoublyLinkedList/DoublyLinkedListTest.java
@@ -11,6 +11,30 @@ public class DoublyLinkedListTest {
 		assertTrue(DLL.isEmpty());
 	}
 	
+	@Test(expected = DoublyLinkedListException.class)
+	public void primerDatoDeNuevaLista() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		DLL.first();
+	}
+	
+	@Test(expected = DoublyLinkedListException.class)
+	public void ultimoDatoDeNuevaLista() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		DLL.last();
+	}
+	
+	@Test(expected = DoublyLinkedListException.class)
+	public void primerNodoDeNuevaLista() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		DLL.firstNode();
+	}
+	
+	@Test(expected = DoublyLinkedListException.class)
+	public void ultimoNodoDeNuevaLista() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		DLL.lastNode();
+	}
+	
 	@Test
 	public void insertaNodoAlPrincipio() throws DoublyLinkedListException {
 		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
@@ -146,13 +170,102 @@ public class DoublyLinkedListTest {
 	}
 	
 	@Test
-	public void insertaNodoDespuesDetrasDeOtro() throws DoublyLinkedListException {
+	public void insertaNodoDespuesDeOtro() throws DoublyLinkedListException {
 		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
-		DoublyLinkedList.Node<Object> node_1 = ;
-		
+		DLL.insertBeginning(new Object());
+		Object node_1 = DLL.first();
+		DoublyLinkedList.Node<Object> linkedNode_1 = DLL.firstNode();
+		Object node_2 = new Object();
+		DoublyLinkedList.Node<Object> linkedNode_2 = new DoublyLinkedList.Node<Object>(node_2, null, null);
+		DLL.insertAfter(linkedNode_1, linkedNode_2);
+		assertEquals(node_1, DLL.first());
+		assertEquals(node_2, DLL.last());
 	}
 	
-	public void insertaNodoAntesDetrasDeOtro() {
-		
+	@Test
+	public void insertaNodoAntesDeOtro() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		DLL.insertBeginning(new Object());
+		Object node_1 = DLL.first();
+		DoublyLinkedList.Node<Object> linkedNode_1 = DLL.firstNode();
+		Object node_2 = new Object();
+		DoublyLinkedList.Node<Object> linkedNode_2 = new DoublyLinkedList.Node<Object>(node_2, null, null);
+		DLL.insertBefore(linkedNode_1, linkedNode_2);
+		assertEquals(node_2, DLL.first());
+		assertEquals(node_1, DLL.last());
+	}
+	
+	@Test
+	public void insertaNodoDespuesDeVarios() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		DLL.insertBeginning(new Object());
+		DLL.insertEnd(new Object());
+		Object node_1 = DLL.first();
+		DoublyLinkedList.Node<Object> linkedNode_2 = DLL.lastNode();
+		Object node_3 = new Object();
+		DoublyLinkedList.Node<Object> linkedNode_3 = new DoublyLinkedList.Node<Object>(node_3, null, null);
+		DLL.insertAfter(linkedNode_2, linkedNode_3);
+		assertEquals(node_1, DLL.first());
+		assertEquals(node_3, DLL.last());
+	}
+	
+	@Test
+	public void insertaNodoAntesDeVarios() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		DLL.insertBeginning(new Object());
+		DLL.insertEnd(new Object());
+		Object node_2 = DLL.last();
+		DoublyLinkedList.Node<Object> linkedNode_1 = DLL.firstNode();
+		Object node_3 = new Object();
+		DoublyLinkedList.Node<Object> linkedNode_3 = new DoublyLinkedList.Node<Object>(node_3, null, null);
+		DLL.insertBefore(linkedNode_1, linkedNode_3);
+		assertEquals(node_3, DLL.first());
+		assertEquals(node_2, DLL.last());
+	}
+	
+	@Test
+	public void insertaNodoEnMitadDeVariosConAfter() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		Object node_1 = new Object();
+		DLL.insertBeginning(node_1);
+		Object node_2 = new Object();
+		DLL.insertEnd(node_2);
+		DoublyLinkedList.Node<Object> linkedNode_1 = DLL.firstNode();
+		DoublyLinkedList.Node<Object> linkedNode_3 = new DoublyLinkedList.Node<Object>(new Object(), null, null);
+		DLL.insertAfter(linkedNode_1, linkedNode_3);
+		assertEquals(node_1, DLL.first());
+		assertEquals(node_2, DLL.last());
+	}
+	
+	@Test
+	public void insertaNodoEnMitadDeVariosConBefore() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		Object node_1 = new Object();
+		DLL.insertBeginning(node_1);
+		Object node_2 = new Object();
+		DLL.insertEnd(node_2);
+		DoublyLinkedList.Node<Object> linkedNode_2 = DLL.lastNode();
+		DoublyLinkedList.Node<Object> linkedNode_3 = new DoublyLinkedList.Node<Object>(new Object(), null, null);
+		DLL.insertBefore(linkedNode_2, linkedNode_3);
+		assertEquals(node_1, DLL.first());
+		assertEquals(node_2, DLL.last());
+	}
+	
+	@Test
+	public void eliminarUltimoNodoDeListaUnitariaYObtenerPrimerNodo() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		Object node_1 = new Object();
+		DLL.insertBeginning(node_1);
+		DLL.deleteLast();
+		assertTrue(DLL.isEmpty());
+	}
+	
+	@Test
+	public void eliminarUltimoNodoDeListaUnitariaYObtenerUltimoNodo() throws DoublyLinkedListException {
+		DoublyLinkedList<Object> DLL = new DoublyLinkedList<Object>();
+		Object node_1 = new Object();
+		DLL.insertBeginning(node_1);
+		DLL.deleteLast();
+		assertTrue(DLL.isEmpty());
 	}
 }
